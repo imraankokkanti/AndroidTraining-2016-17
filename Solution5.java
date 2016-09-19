c72068e4a13b249ae30da5a563b1eb7490347b43
Imraan Kokkanti <imraankokkanti@gmail.com>
2016-09-19 18:26:25 +0530

    Delete Solution5.java

--- Solution5.java
+++
@@ -1,7 +0,0 @@
-public class Solution5{
-	public String makeTags(String tag,String dat){
-		StringBuilder sb=new StringBuilder("<");
-		sb.append(tag).append(">").append(dat).append("</").append(tag).append(">");
-		return sb.toString();
-	}
-}
