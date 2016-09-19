1f65529f5fd0659e7778cdea0885c0ea15841d9b
Imraan Kokkanti <imraankokkanti@gmail.com>
2016-09-19 18:23:21 +0530

    Delete TestSolution3.java

--- TestSolution3.java
+++
@@ -1,9 +0,0 @@
-public class TestSolution3{
-	public static void main(String []args){
-		Solution3 ep=new Solution3();
-		if(ep.containE("Hello"))
-			System.out.println("true");
-		else
-			System.out.println("false");
-	}
-}
