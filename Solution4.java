531c47d5dba3b63b66767a92d5ff2eaeb20a0c18
Imraan Kokkanti <imraankokkanti@gmail.com>
2016-09-19 18:26:18 +0530

    Delete Solution4.java

--- Solution4.java
+++
@@ -1,8 +0,0 @@
-public class Solution4{
-	public String embedWord(String a, String b){
-		StringBuilder sb=new StringBuilder(a);
-		int offset=(sb.length()/2);
-		sb.insert(offset,b);
-		return sb.toString();
-	}
-}
