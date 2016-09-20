8316c31d4dd2e5c44d741b044d91737844e80985
Imraan Kokkanti <imraankokkanti@gmail.com>
2016-09-20 13:01:34 +0530

    Update Solution2.java

--- 19-09-2016/Solution2.java
+++ 19-09-2016/Solution2.java
@@ -1,7 +1,8 @@
 public class Solution2{
 	public int maxBlock(String s){
 		int max=1,temp=1;
-		
+		if(s.length()==0)
+			return 0;
 		for(int i=0;i<s.length()-1;i++){
 			if(s.substring(i,i+1).equals(s.substring(i+1,i+2))){
 				temp++;
